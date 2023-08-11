dba085ffe26bbf597b1f087632dea16d0f5c4756
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2023-08-11 22:53:12 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+//navbar color
+var navbar = document.getElementById('navbar');
+
+document.addEventListener("scroll", () => {
+    if (window.scrollY > 0) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+//multiple text
+const text = document.querySelector(".multiple-text");
+const textLoad = () => {
+    setTimeout(() => {
+        text.textContent = "Front-end Web Developer";
+    }, 0);
+    setTimeout(() => {
+        text.textContent = "UI/UX Designer";
+    }, 1500);
+    setTimeout(() => {
+        text.textContent = "Photographer";
+    }, 3000);
+}
+textLoad();
+setInterval(textLoad, 4500);
+
+
+//skill animation
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            observer.unobserve(entry.target);
+            var percentVal = 0;
+            var finalVal = parseInt(entry.target.getAttribute('aria-valuenow'));
+            console.log(percentVal);
+
+            setInterval(function () {
+                if (percentVal < finalVal) {
+                    percentVal += 1;
+                    entry.target.style.width = percentVal + '%';
+                    entry.target.setAttribute('aria-valuenow', percentVal);
+                }
+            }, 0.1);
+        }
+    });
+}, {
+    root: null,
+    rootMargin: '0px',
+    threshold: 1
+});
+
+const targets = document.querySelectorAll('.progress-bar');
+targets.forEach(target => observer.observe(target));
+
+
+
+//hide navbar
+
+// var prevScrollpos = window.pageYOffset;
+// window.onscroll = function() {
+// var currentScrollPos = window.pageYOffset;
+//   if (prevScrollpos > currentScrollPos) {
+//     document.getElementById("navbar").style.top = "0px";
+//   } else {
+//     document.getElementById("navbar").style.top = "0px";
+//   }
+//   prevScrollpos = currentScrollPos;
+// }
+
+
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.header-img,.project-title,.service-title', { origin: 'top' });
+ScrollReveal().reveal('.img-about,.h6-head,.description-hire-button', { origin: 'right' });
+ScrollReveal().reveal('.description-about', { origin: 'top' });
+ScrollReveal().reveal('.prjct,.srvc', { origin: 'bottom' });
+ScrollReveal().reveal('.h1-head,.description-hire', { origin: 'left' });
