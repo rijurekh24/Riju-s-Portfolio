6fd36ca4ba4cd27e8e2e59d2a1f117add3016781
Rijurekh Ghosh <93432199+rijurekh24@users.noreply.github.com>
2023-08-14 01:52:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,38 +1,30 @@
+//preloader animation
+$(window).on('load', function () {
+    setTimeout(function () {
+        $('.preloader').addClass('preloader-deactivate');
+    }, 2000);
+
+    setTimeout(function () {
+        $('.slide').addClass('slide-deactivate');
+    }, 2200);
+});
+
 
 //hamburger sign
 
-let btn=document.querySelector('.navbar-toggler');
-let icon=btn.querySelector('.fa-bars');
+let btn = document.querySelector('.navbar-toggler');
+let icon = btn.querySelector('.fa-bars');
 
-btn.onclick=function()
-{
-    if(icon.classList.contains("fa-bars"))
-    {
-        icon.classList.replace("fa-bars","fa-xmark");
+btn.onclick = function () {
+    if (icon.classList.contains("fa-bars")) {
+        icon.classList.replace("fa-bars", "fa-xmark");
     }
-    else{
-        icon.classList.replace("fa-xmark","fa-bars");
+    else {
+        icon.classList.replace("fa-xmark", "fa-bars");
     }
 }
 
-//navbar active
-// let sections=document.querySelectorAll('section');
-// let navLinks=document.querySelectorAll('.navbar-nav .nav-item a');
-// window.onscroll = () => {
-//     sections.forEach(sec => {
-//         let top=window.scrollY;
-//         let offset=sec.offsetTop;
-//         let height=sec.offsetHeight;
-//         let id=sec.getAttribute('id');
 
-//         if(top >= offset && top < offset + height){
-//             navLinks.forEach(links =>{
-//                 links.classList.remove('active');
-//                 document.querySelector('.navbar-nav .nav-item a[href*=' + id + ']').classList.add('active');
-//             })
-//         }
-//     });
-// };
 
 //navbar color
 var navbar = document.getElementById('navbar');
@@ -70,16 +62,17 @@ const observer = new IntersectionObserver((entries) => {
             observer.unobserve(entry.target);
             var percentVal = 0;
             var finalVal = parseInt(entry.target.getAttribute('aria-valuenow'));
-            console.log(percentVal);
+            // console.log(percentVal);
 
             setInterval(function () {
                 if (percentVal < finalVal) {
-                    percentVal += 0.7;
+                    percentVal += 0.5;
                     entry.target.style.width = percentVal + '%';
                     entry.target.setAttribute('aria-valuenow', percentVal);
                 }
-            }, 0.5);
+            }, 0.1);
         }
+
     });
 }, {
     root: null,
@@ -99,17 +92,52 @@ ScrollReveal({
     delay: 200
 });
 
+
 ScrollReveal().reveal('.project-title,.service-title,.description-about,.skills-title,.contact-title,.gallery-title,.education-title,hr,.hire-title', { origin: 'top' });
 ScrollReveal().reveal('.img-about,.h6-head,.description-hire-button,.email,.gallery-img', { origin: 'right' });
 ScrollReveal().reveal('.srvc,.text,.edc', { origin: 'bottom' });
 ScrollReveal().reveal('.h1-head,.hire-description,.name,.gallery-about', { origin: 'left' });
 
-
-ScrollReveal().reveal('.header-img', { scale: 1.7});
-ScrollReveal().reveal('.prjct', { scale: 0.5,duration:1500,interval:200});
+ScrollReveal().reveal('.header-img', { scale: 1.7 });
+ScrollReveal().reveal('.prjct', { scale: 0.5, duration: 1500, interval: 200 });
 ScrollReveal().reveal('.srvc', { interval: 150 });
 
-ScrollReveal().reveal('.hire-description', { delay: 300});
+ScrollReveal().reveal('.hire-description', { delay: 300 });
+ScrollReveal().reveal('.fa-brands', { origin: 'bottom', delay: 500, interval: 150, distance: '30px' });
+ScrollReveal().reveal('.h1-head,.h6-head,.header-img', { delay: 2250, reset: false });
 
-ScrollReveal().reveal('.fa-brands', { origin:'bottom',delay:500,interval: 150,distance:'30px'});
-ScrollReveal().reveal('.h1-head,.h6-head,.header-img', { reset: false});
+
+//mode  light dark
+var navbar = document.querySelector('#navbar');
+var about = document.querySelector('#about');
+var skills = document.querySelector('#skills');
+var hire = document.querySelector('#hire');
+var gallery = document.querySelector('#gallery');
+var footer = document.querySelector('#FOOTER');
+var education = document.querySelector('#education');
+var services = document.querySelector('#services');
+var projects = document.querySelector('#projects');
+var contact = document.querySelector('#contact');
+var theme = document.querySelector('.theme');
+var icn = theme.querySelector('.fa-moon');
+
+function changeTheme() {
+    navbar.classList.toggle('dark');
+    about.classList.toggle('dark')
+    skills.classList.toggle('dark')
+    hire.classList.toggle('dark')
+    gallery.classList.toggle('dark')
+    footer.classList.toggle('dark')
+    education.classList.toggle('dark2')
+    services.classList.toggle('dark2')
+    projects.classList.toggle('dark2')
+    contact.classList.toggle('dark2')
+
+    if (icn.classList.contains("fa-moon")) {
+        icn.classList.replace("fa-moon", "fa-sun");
+    }
+    else {
+        icn.classList.replace("fa-sun", "fa-moon");
+    }
+}
+
